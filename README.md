4787637abbaacb99e0ed44af775c6b16b8344da7
Patrick Donnelly <pdonnelly92@gmail.com>
2019-11-21 11:54:37 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Donnelly_2019_SSR
-This is a repository including data, analysis scripts, and figure generation scripts associated with the following manuscript, "Annotating digital text with phonemic cues to support decoding performance in struggling readers"
+This is a repository including data, analysis scripts, and figure generation scripts associated with the following manuscript, "Annotating digital text with phonemic cues to support decoding performance in struggling readers" submitted to PsyRxiv.
+
+Citation: Donnelly, P. M., Larson, K., Matskewich, T., & Yeatman, J. D. (2019, November 19). Annotating digital text with phonemic cues to support decoding in struggling readers. https://doi.org/10.31234/osf.io/7mw5b
+
+Datasets are available in the data folder.
+
+figures.ipynb is a Jupyter notebook that will generate figures from the manuscript.
+lmeanalysis.m is a matlab analysis script that will perform the analyses from the manuscript.
+
+Requirements for analysis: Matlab software & Python 3
+
+Please direct all questions to Donnelly: pdonne(@)u.washington.edu
---
+++ lme_analysis.m
@@ -0,0 +1,75 @@
+%% LME Analysis Script for Donnelly 2019 SSR
+% Patrick M. Donnelly
+% University of Washington
+% November 21, 2019
+
+%% Data processing
+% read in data 
+data = readtable('data/matlab_data.csv');
+
+% rename variables for ease in analysis
+data.Properties.VariableNames = {'Var1', 'id', 'session', 'group', ...
+    'study_name', 'word1_acc', 'word2_acc', 'pseudo1_acc', ...
+    'pseudo2_acc', 'first_acc', 'second_rate', 'wj_brs', 'twre_index', ...
+    'ctopp_rapid','practice'};
+
+% make time and group variables categorical
+categorical(data.session);
+categorical(data.group);
+
+% calculate difference scores and create new variables
+data.worddiff = data.word2_acc - data.word1_acc;
+data.pseudodiff = data.pseudo2_acc - data.pseudo1_acc;
+
+%extend practice variable to both visits for LME analysis
+for sub = 1:length(data.id)
+   if isnan(data.practice(sub))
+       data.practice(sub) = data.practice(sub+1);
+   end
+end
+
+% create new stacked dataset for wordlist data analysis
+data_stacked = stack(data,{'word1_acc','word2_acc',...
+    'pseudo1_acc','pseudo2_acc'},'NewDataVariableName','acc');
+data_stacked.type = data_stacked.acc_Indicator == 'word1_acc' | ...
+    data_stacked.acc_Indicator=='word2_acc';
+
+%% LME analysis {preregistered}
+
+%% Real word analysis
+
+model = 'acc ~ 1 + group*session + (1-session|id)+ (1|acc_Indicator)';
+
+% focus in on dataset
+real_data = data_stacked(data_stacked.type==true,:);
+
+% run model fit
+lme_realword = fitlme(real_data, model, 'FitMethod', 'ML');
+
+%% Pseudo word analysis
+
+model = 'acc ~ 1 + group*session + (1-session|id)+ (1|acc_Indicator)';
+
+pseudo_data = data_stacked(data_stacked.type==false,:);
+
+% run model fits
+lme_pseudoword = fitlme(pseudo_data, model, 'FitMethod', 'ML');
+
+%% Passage data
+
+% accuracy model
+model = 'first_acc ~ 1 + group*session + (1-session|id)'
+
+% run model fits
+lme_accuracy = fitlme(data, model, 'FitMethod', 'ML');
+
+% rate model
+model = 'second_rate ~ 1 + group*session + (1-session|id)'
+
+lme_rate = fitlme(data, model, 'FitMethod', 'ML')
+
+
+
+
+
+
